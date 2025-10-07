02bc204724a31da11b6927bbeda6fa142b6abb3e
Amazingharman2 <chpudas@gmail.com>
2025-10-07 19:00:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,25 +12,25 @@ id_pattern = re.compile(r'^-?\d+$')
 
 
 SESSION = environ.get("SESSION", "UHDFiletoLinksBot")
-API_ID = int(environ.get("API_ID", "0"))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "23991460"))
+API_HASH = environ.get("API_HASH", "482b9c11ca28fdff8f0d3f9223ef0ac1")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7556995516:AAGEVw6A1dm-eW-sOU5Ak_8xghk4Y6_Y744")
 
 
 PORT = int(environ.get("PORT", "8080"))
 MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "600"))  # 20 minutes
 ON_HEROKU = "DYNO" in environ
 URL = environ.get("URL", "")
 
 
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "0"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1003143688977"))
 ADMINS = [
     int(admin) if id_pattern.match(admin) else admin
     for admin in environ.get("ADMINS", "").split()
 ]
 
 
-DATABASE_URI = environ.get("DATABASE_URI", "")
-DATABASE_NAME = environ.get("DATABASE_NAME", "")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://chpudas:SPNjedirQ26Nungu@clustercompressbot.qnztune.mongodb.net/?retryWrites=true&w=majority&appName=Clustercompressbot")
+DATABASE_NAME = environ.get("DATABASE_NAME", "Clustercompressbot")
